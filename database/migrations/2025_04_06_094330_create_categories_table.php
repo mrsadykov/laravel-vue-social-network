88ddc5d0b91f686114c4fcfb268e5131fbd11d31
iskandarsadykov <mrsadykov96@gmail.com>
2025-11-17 17:46:44 +0500

    fix categories table migration title index error

--- database/migrations/2025_04_06_094330_create_categories_table.php
+++ database/migrations/2025_04_06_094330_create_categories_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->text('string')->index();
         });
     }
 
