253ea3109bc598b83e35e4d3715503f832728cdc
iskandarsadykov <mrsadykov96@gmail.com>
2025-11-17 17:48:38 +0500

    fix categories table migration title index error

--- database/migrations/2025_04_06_094330_create_categories_table.php
+++ database/migrations/2025_04_06_094330_create_categories_table.php
@@ -4,26 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-/**
- * Создание таблицы
- */
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 500); // До 500 символов
             $table->timestamps();
-            $table->text('string')->index();
+
+            // Теперь можно безопасно создать индекс
+            $table->index('title');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('categories');
